310fbd7ef0b867d3cd46f7bb05e91b9c7e18a1fc
yoh-wi <57764555+yoh-wi@users.noreply.github.com>
2019-12-03 01:23:18 +0900

    user-registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+<<<<<<< Updated upstream
   root to: "messages#index"
 end
+=======
+  root to: 'messages#index'
+  resources :users, only: [:edit, :update]
+end
+>>>>>>> Stashed changes
